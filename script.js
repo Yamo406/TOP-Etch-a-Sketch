4ff8d4bb026bad14841128594f883ca48f843cbd
Yamo406 <93231355+Yamo406@users.noreply.github.com.>
2024-12-24 12:34:32 +0200

    add random colors

--- script.js
+++ script.js
@@ -10,5 +10,11 @@ for (let i = 0; i < 16; i++) {
 
 container.addEventListener("mouseover", (event) => {
     let target = event.target.closest(".gridBox");
-    target.classList.add('active');
+    target.style.backgroundColor =
+        "#" +
+        Math.floor(Math.random() * 16777215)
+            .toString(16)
+            .padStart(6, "0");
+
+    // target.classList.add('active');
 });
