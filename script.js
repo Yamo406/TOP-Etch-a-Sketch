827b3cfb0b3822575f87f06a2094fe697bcab063
Yamo406 <93231355+Yamo406@users.noreply.github.com.>
2024-12-24 23:27:43 +0200

    finalize square sizes issue

--- script.js
+++ script.js
@@ -5,26 +5,33 @@ const container = document.querySelector(".container");
 // Variables
 let squaresNumber = 16;
 
-// square numbers button
-squareButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    squaresNumber = Number(prompt(
-        "Enter the Number of squares you want per side (max 100)", 16
-    ));
-    if (squaresNumber > 100) {
-        alert("Can't have more than 100 squares per side");
-        squaresNumber = 16;
-    }
-});
+function createGrid(size) {
+    container.innerHTML = "";
 
-// create the squares
-for (let i = 0; i < squaresNumber; i++) {
-    for (let j = 0; j < squaresNumber; j++) {
+    container.style.setProperty("--grid-size", size);
+
+    // create the squares
+    for (let i = 0; i < size * size; i++) {
         const div = document.createElement("div");
         div.classList.add("gridSquare");
         container.appendChild(div);
     }
 }
+createGrid(squaresNumber);
+
+// square numbers button
+squareButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let newSize = Number(
+        prompt("Enter the Number of squares you want per side (max 100)")
+    );
+    if (newSize > 100 || newSize === NaN || newSize === null || newSize === 0) {
+        alert("Can't have more than 100 squares per side");
+        newSize = 16;
+    }
+    squaresNumber = newSize;
+    createGrid(squaresNumber);
+});
 
 // helper function to add Event delegation
 function addGlobalEventListener(type, selector, callback, parent = document) {
@@ -44,6 +51,15 @@ addGlobalEventListener(
             Math.floor(Math.random() * 16777215)
                 .toString(16)
                 .padStart(6, "0");
+    },
+    container
+);
+
+addGlobalEventListener(
+    "click",
+    ".gridSquare",
+    (e) => {
+        e.target.style.backgroundColor = "white";
     },
     container
 );
